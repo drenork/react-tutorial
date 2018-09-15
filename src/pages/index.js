39fefdb457ec1f813dab486c15a72a4b62c3d28a
David Kroner <david.kroner@betterment.com>
2018-09-14 23:05:55 -0400

    add intro transitions to hero, hover transitions to buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Reach & Gatsby</h1>
+        <h1>React & Gatsby</h1>
         <p>
           Working through some tutorials to learn how to design and code using
           React and Gatsby framework, which seems pretty straighforward so far.
