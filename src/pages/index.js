8ac163cfcdeefa9e9eeeca3f4dbedfb26ac0c258
David Kroner <david.kroner@betterment.com>
2018-09-14 21:52:50 -0400

    add all gatsby files

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learning to</h1>
+    <p>design and code React</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
